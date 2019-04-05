b2120112635a59fd44813a6df5852b701a74b5c1
golemheavy <steve@gridtop.com>
2019-04-05 14:19:06 -0500

    improving functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,24 @@
 let gameOver = false;
+let targetDiv = null;
+
+
+var textElements = {
+	selectedAnswerText : document.getElementById("selected-answer")
+};
 
 
 function clickListener(event) {
-	if (gameOver) {document.removeEventListener('click', clickListener); return; }
+	if (gameOver) { document.removeEventListener('click', clickListener); return; } // also need to display game over message
 	var clickedValue = event.target;
-	console.log(clickedValue);
+	if (clickedValue.attributes[1]) {
+		if (clickedValue.attributes[1].value.includes("choice-btn")) console.log(true);
+		if (clickedValue.attributes[0]) {
+			targetDiv = document.getElementById(clickedValue.attributes[0].value);
+			console.log(targetDiv.innerHTML);
+			textElements.selectedAnswerText.textContent = "You selected: " + targetDiv.innerHTML;
+			
+		}
+	}
 }
 
 document.addEventListener('click', clickListener);
